b53875a21068cd589324ef15a1984695651ceade
Amir Valibeigi <amir.av79@gmail.com>
2021-11-29 16:18:14 +0330

    add timer counterdown add weapon choose add icon for attack, reload, pickup

---
+++ CsMini/Assets/src/Dust/Scripts/UI/Buttons/JoyButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    [HideInInspector]
+    public bool pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+}
