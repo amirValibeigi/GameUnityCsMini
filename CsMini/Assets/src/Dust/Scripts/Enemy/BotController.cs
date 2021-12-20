0d7615fd872b9e7ddab5df00b9b3b7b08845269a
Amir Valibeigi <amir.av79@gmail.com>
2021-12-20 19:30:19 +0330

    add camps for bots,fix animation stop near target

--- CsMini/Assets/src/Dust/Scripts/Enemy/BotController.cs
+++ CsMini/Assets/src/Dust/Scripts/Enemy/BotController.cs
@@ -7,33 +7,47 @@ public class BotController : MonoBehaviour
 {
     private NavMeshAgent agent = null;
     private Animator animator;
-    private Transform target;
+    private Transform targetPlayer;
+    private Transform targetCamp;
+    private EnemyCamp enemyCamp;
 
     private void Start()
     {
         getReferences();
+        initVarables();
     }
 
     private void Update()
     {
-        moveToTarget();
+        moveToRandomCamp();
     }
 
-    private void moveToTarget()
+
+    private void moveToRandomCamp()
+    {
+        if (isNearTarget(5f))
+        {
+            moveToTarget(targetPlayer);
+            return;
+        }
+
+        moveToTarget(targetCamp);
+    }
+
+    private void moveToTarget(Transform target)
     {
         agent.SetDestination(target.position);
         animator.SetFloat("Speed", 1f, 0.3f, Time.deltaTime);
 
-        rotateToTarget();
+        rotateToTarget(target);
 
-        if (agent.isStopped)
+        if (isNearTarget(target))
         {
             animator.SetFloat("Speed", 0f);
         }
-
     }
 
-    private void rotateToTarget()
+    private void rotateToTarget(Transform target)
     {
         Vector3 direction = target.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
@@ -42,11 +56,40 @@ public class BotController : MonoBehaviour
         transform.rotation = rotation;
     }
 
+
+    private bool isNearTarget()
+    {
+        return isNearTarget(targetPlayer, agent.stoppingDistance);
+    }
+
+    private bool isNearTarget(float stoppingDistance)
+    {
+        return isNearTarget(targetPlayer, stoppingDistance);
+    }
+
+    private bool isNearTarget(Transform target)
+    {
+        return isNearTarget(target, agent.stoppingDistance);
+    }
+
+    private bool isNearTarget(Transform target, float stoppingDistance)
+    {
+        float distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        return distanceToTarget <= stoppingDistance;
+    }
+
     private void getReferences()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
-        target = PlayerMovement.instance.transform;
+        targetPlayer = PlayerMovement.instance.transform;
+        enemyCamp = FindObjectOfType<EnemyCamp>();
     }
 
+    private void initVarables()
+    {
+        targetCamp = enemyCamp.getCamp();
+        Debug.Log(targetCamp.position);
+    }
 }
